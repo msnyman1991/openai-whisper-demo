a7472d455d67b945fbb3353f00e1acc286a2432d
Morne Snyman <msnyman@mobiquityinc.com>
2023-09-06 13:28:50 +0200

    added infra code and updated pipeline

--- infrastructure/tf-modules/aci/variables.tf
+++ infrastructure/tf-modules/aci/variables.tf
@@ -49,7 +49,7 @@ variable "memory_in_gb" {
 variable "restart_policy" {
   type        = string
   description = "The behavior of Azure runtime if container has stopped."
-  default     = "Always"
+  default     = "Never"
   validation {
     condition     = contains(["Always", "Never", "OnFailure"], var.restart_policy)
     error_message = "The restart_policy must be one of the following: Always, Never, OnFailure."
