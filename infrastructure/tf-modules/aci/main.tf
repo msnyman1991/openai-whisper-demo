a293f3e571956d7a592c836b342cb7315d84c854
Morne Snyman <msnyman@mobiquityinc.com>
2023-09-06 13:01:22 +0200

    added infra code and updated pipeline

--- infrastructure/tf-modules/aci/main.tf
+++ infrastructure/tf-modules/aci/main.tf
@@ -22,6 +22,12 @@ resource "azurerm_container_group" "container" {
   os_type             = "Linux"
   restart_policy      = var.restart_policy
 
+  image_registry_credential {
+    username = "whisper-registry"
+    password = "jgpfaqrffYl8J5DU/XODUfM22oa5HNsYlBfJRhQSfG+ACRB0Qst+"
+    server   = "whisperujwrf.azurecr.io"
+  }
+
   container {
     name   = "${var.container_name_prefix}-${random_string.container_name.result}"
     image  = var.image
