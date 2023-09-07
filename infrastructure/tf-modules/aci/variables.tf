2a541eb6be96f3b9dcd7409ec8ca47b0fc124492
Morne Snyman <msnyman@mobiquityinc.com>
2023-09-07 11:02:55 +0200

    added loabbalancer

--- infrastructure/tf-modules/aci/variables.tf
+++ infrastructure/tf-modules/aci/variables.tf
@@ -67,3 +67,31 @@ variable "acr_password" {
 variable "acr_servername" {
   type = string
 }
+
+variable "lb_resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "public_ip_name" {
+  type = string
+}
+
+variable "loadbalancer_name" {
+  type = string
+}
+
+variable "lb_frontend_name" {
+  type = string
+}
+
+variable "lb_backend_pool_name" {
+  type = string
+}
+
+variable "port" {
+  type = number
+}
