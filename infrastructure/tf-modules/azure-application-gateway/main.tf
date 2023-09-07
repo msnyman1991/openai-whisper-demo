562519b5036b6e1bb9bd0494cb82193fddcc99e3
Morne Snyman <msnyman@mobiquityinc.com>
2023-09-07 16:22:56 +0200

    added loabbalancer

--- infrastructure/tf-modules/azure-application-gateway/main.tf
+++ infrastructure/tf-modules/azure-application-gateway/main.tf
@@ -41,10 +41,10 @@ resource "azurerm_application_gateway" "this" {
   }
 
   http_listener {
-    name                      = "HttpListener"
-    frontend_ip_configuration = var.app_gateway_frontend_ip_config_name
-    frontend_port             = var.app_gateway_frontend_port_name
-    protocol                  = "Http"
+    name                           = "HttpListener"
+    frontend_ip_configuration_name = var.app_gateway_frontend_ip_config_name
+    frontend_port_name             = var.app_gateway_frontend_port_name
+    protocol                       = "Http"
   }
 
   request_routing_rule {
