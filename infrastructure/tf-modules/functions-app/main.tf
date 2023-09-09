ea392d2cfe45a5215b1c4c47fc97ea30e0f55f10
Morne Snyman <msnyman@mobiquityinc.com>
2023-09-09 18:00:52 +0200

    added azure function

--- infrastructure/tf-modules/functions-app/main.tf
+++ infrastructure/tf-modules/functions-app/main.tf
@@ -31,54 +31,54 @@ resource "azurerm_linux_function_app" "this" {
   app_settings = {
     "FUNCTIONS_EXTENSION_VERSION"                     = "~4"
     "FUNCTIONS_WORKER_RUNTIME"                        = "python"
-    "AzureFunctionsJobHost__version"                  = "2.0"
-    "AzureFunctionsJobHost__extensionBundle__id"      = "Microsoft.Azure.Functions.ExtensionBundle"
-    "AzureFunctionsJobHost__extensionBundle__version" = "[3.*, 4.0.0)"
-    "MY_FUNCTION_CODE"                                = <<-EOT
-     import azure.functions as func
-     from azure.identity import DefaultAzureCredential
-     from azure.mgmt.containerinstance import ContainerInstanceManagementClient
-     from azure.mgmt.containerinstance.models import ContainerGroup, Container
+    # "AzureFunctionsJobHost__version"                  = "2.0"
+    # "AzureFunctionsJobHost__extensionBundle__id"      = "Microsoft.Azure.Functions.ExtensionBundle"
+    # "AzureFunctionsJobHost__extensionBundle__version" = "[3.*, 4.0.0)"
+    # "MY_FUNCTION_CODE"                                = <<-EOT
+    #  import azure.functions as func
+    #  from azure.identity import DefaultAzureCredential
+    #  from azure.mgmt.containerinstance import ContainerInstanceManagementClient
+    #  from azure.mgmt.containerinstance.models import ContainerGroup, Container
 
-     def main(req: func.HttpRequest) -> func.HttpResponse:
-         # Authenticate using Azure AD credentials or other supported methods
-         credential = DefaultAzureCredential()
-         aci_client = ContainerInstanceManagementClient(credential, "afad4717-6a47-4784-93ed-1c7992b70984")
+    #  def main(req: func.HttpRequest) -> func.HttpResponse:
+    #      # Authenticate using Azure AD credentials or other supported methods
+    #      credential = DefaultAzureCredential()
+    #      aci_client = ContainerInstanceManagementClient(credential, "afad4717-6a47-4784-93ed-1c7992b70984")
 
-         container_group_name = "whisper_acigroup"
-         container_name = "whisper-1"
-         container_image = "whisper.azurecr.io/whisper.azurecr.io/whisper-image:latest"
-         container_resource_requests = {
-             "cpu": "0.5",
-             "memory_in_gb": "1.5"
-         }
+    #      container_group_name = "whisper_acigroup"
+    #      container_name = "whisper-1"
+    #      container_image = "whisper.azurecr.io/whisper.azurecr.io/whisper-image:latest"
+    #      container_resource_requests = {
+    #          "cpu": "0.5",
+    #          "memory_in_gb": "1.5"
+    #      }
 
-         container_ports = [
-              {
-                  "port": 9000
-              }
-          ]
+    #      container_ports = [
+    #           {
+    #               "port": 9000
+    #           }
+    #       ]
 
-         container = Container(
-             name=container_name,
-             image=container_image,
-             resources=container_resource_requests
-         )
+    #      container = Container(
+    #          name=container_name,
+    #          image=container_image,
+    #          resources=container_resource_requests
+    #      )
 
-         container_group_properties = ContainerGroup(
-             os_type="Linux",
-             containers=[container],
-             location="${var.location}",
-         )
+    #      container_group_properties = ContainerGroup(
+    #          os_type="Linux",
+    #          containers=[container],
+    #          location="${var.location}",
+    #      )
 
-         aci_client.container_groups.create_or_update(
-             resource_group_name="whisper_rg-top-loon",
-             container_group_name=container_group_name,
-             container_group=container_group_properties
-         )
+    #      aci_client.container_groups.create_or_update(
+    #          resource_group_name="whisper_rg-top-loon",
+    #          container_group_name=container_group_name,
+    #          container_group=container_group_properties
+    #      )
 
-         return func.HttpResponse("Container instance created successfully.")
-     EOT
+    #      return func.HttpResponse("Container instance created successfully.")
+    #  EOT
   }
   site_config {
     application_stack {
